70bbc86eac7d0ec85510b784f4d1bbc81ab26d8c
Amanda McDermott <amanda@tidelift.com>
2020-09-18 21:12:22 -0400

    Handle winning states

--- src/index.js
+++ src/index.js
@@ -30,6 +30,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.symbol;
     this.setState({
       squares: squares,
@@ -52,7 +55,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.symbol}`;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.symbol}`;
+    }
 
     return (
       <div>
@@ -99,3 +108,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const winningSets = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < winningSets.length; i++) {
+    const [a, b, c] = winningSets[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
